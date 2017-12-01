9b86e2ec8fad668607ab143cbbae21fa33b2bb7a
Kyle <kyle.sims@comcast.net>
2017-12-01 09:07:11 +0000

    Day 2 template

---
+++ 2/2.py
@@ -0,0 +1,14 @@
+def solve(inp):
+    return None
+
+
+def main():
+    with open('input.txt') as f:
+        puzzle_input = f.readline()
+
+    print(solve(puzzle_input))
+    # print(solve(puzzle_input))
+
+
+if __name__ == '__main__':
+    main()
