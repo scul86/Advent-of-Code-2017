345de5ab069c360d6ab9d527b105e3eaa379e619
Kyle <kyle.sims@comcast.net>
2017-12-01 08:58:22 +0000

    Day 1

--- 1/1.py
+++ 1/1.py
@@ -1,8 +1,14 @@
-with open('input.txt') as f:
-    puzzle_input = f.readline()
+def solve(inp, offset):
+    return sum(int(n1) for n1, n2 in zip(inp, inp[offset:] + inp[:offset]) if n1 == n2)
 
-print(sum(int(n1) for n1, n2 in zip(puzzle_input, puzzle_input[1:]+puzzle_input[0]) if n1 == n2))
 
-half_len = len(puzzle_input) // 2
+def main():
+    with open('input.txt') as f:
+        puzzle_input = f.readline()
 
-print(sum(int(n1) for n1, n2 in zip(puzzle_input, puzzle_input[half_len:] + puzzle_input[:half_len]) if n1 == n2))
+    print(solve(puzzle_input, 1))
+    print(solve(puzzle_input, len(puzzle_input)//2))
+
+
+if __name__ == '__main__':
+    main()
