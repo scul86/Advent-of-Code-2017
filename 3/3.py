8a9c900a81c5771ada31a6c46aa8623eff73f1fe
Kyle <kyle.sims@comcast.net>
2017-12-02 05:30:14 +0000

    Day 3 template

---
+++ 3/3.py
@@ -0,0 +1,16 @@
+def solve_p1(inp):
+    pass
+
+def solve_p2(inp):
+    pass
+
+def main():
+    with open('input.txt') as f:
+        puzzle_input = f.read().splitlines()
+
+    print(solve_p1(puzzle_input))
+    print(solve_p2(puzzle_input))
+
+
+if __name__ == '__main__':
+    main()
